0ec6aa6786bb8f8e2716dea48d44991a8b0c87ab
Antonio Todorov <at.antonio.todorov@gmail.com>
2024-01-09 14:47:44 +0200

    summer outfit upload

---
+++ Python_basics/Conditional Statements Advanced Exercise/02_summer_outfit.py
@@ -0,0 +1,36 @@
+degrees = int(input())
+time_of_day = input()
+
+outfit = ""
+shoes = ""
+if time_of_day == "Morning":
+    if degrees >= 10 and degrees <= 18:
+        outfit = "Sweatshirt"
+        shoes = "Sneakers"
+    elif degrees > 18 and degrees <= 24:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif degrees > 24:
+        outfit = "T-Shirt"
+        shoes = "Sandals"
+elif time_of_day == "Afternoon":
+    if degrees >= 10 and degrees <= 18:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif degrees > 18 and degrees <= 24:
+        outfit = "T-Shirt"
+        shoes = "Sandals"
+    elif degrees > 24:
+        outfit = "Swim Suit"
+        shoes = "Barefoot"
+elif time_of_day == "Evening":
+    if degrees >= 10 and degrees <= 18:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif degrees > 18 and degrees <= 24:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif degrees > 24:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
