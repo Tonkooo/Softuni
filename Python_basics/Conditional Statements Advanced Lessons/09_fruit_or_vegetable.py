ffe7658e4c3415235524ce2ccc1cb7a8da7d27bb
Antonio Todorov <at.antonio.todorov@gmail.com>
2024-01-23 22:25:15 +0200

    fruit_or_vegetable_code_upload

---
+++ Python_basics/Conditional Statements Advanced Lessons/09_fruit_or_vegetable.py
@@ -0,0 +1,8 @@
+enter = input()
+
+if enter == "banana" or enter == "apple" or enter == "kiwi" or enter == "cherry" or enter == "lemon" or enter == "grapes":
+    print("fruit")
+elif enter == "tomato" or enter == "cucumber" or enter == "pepper" or enter == "carrot":
+    print("vegetable")
+else:
+    print("unknown")
