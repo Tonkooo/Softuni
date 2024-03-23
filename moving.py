020a67e8a4a6c9812aa5676d7e61721364f5463e
Antonio Todorov <at.antonio.todorov@gmail.com>
2024-03-23 07:25:26 +0200

    moving_code

---
+++ moving.py
@@ -0,0 +1,20 @@
+width = int(input())
+length = int(input())
+height = int(input())
+
+volume = width * length * height
+
+input_line = input()
+sum_boxes = 0
+while input_line != "Done":
+    boxes = int(input_line) #ако не е Done, то тогава е брой кашони
+    sum_boxes += boxes
+    if sum_boxes >= volume:
+        break
+
+    input_line = input()
+diff = abs(volume - sum_boxes)
+if sum_boxes > volume:
+    print(f"No more free space! You need {diff} Cubic meters more.")
+else:
+    print(f"{diff} Cubic meters left.")
